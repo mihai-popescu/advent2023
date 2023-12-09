1b76a31eb0da806a5ae6fa60d4485892c07d2021
Mihai Popescu <popescu.mihaidan@gmail.com>
2023-12-09 13:47:28 +0200

    Day 1 refact

--- src/main/kotlin/Advent1.kt
+++ src/main/kotlin/Advent1.kt
@@ -1,42 +1,27 @@
+import kotlin.math.max
+
 private val digitStrings =
     listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 private val digitChar = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
 
-fun advent1p1(input: String): Long {
-    return input.lowercase().split("\n").fold(0L) { acc, s ->
-        val firstDigit = when (val charDigit = s.findAnyOf(digitChar)) {
-            null -> 0
-            else -> digitChar.indexOf(charDigit.second)
-        }
-        val lastDigit = when (val lastCharDigit = s.findLastAnyOf(digitChar)) {
-            null -> 0
-            else -> digitChar.indexOf(lastCharDigit.second)
-        }
-        acc + firstDigit * 10 + lastDigit
+fun advent1p1(input: String): Long =
+    input.lowercase().split("\n").fold(0L) { acc, s ->
+        acc + ((s.findAnyOf(digitChar)?.second ?: "") + (s.findLastAnyOf(digitChar)?.second ?: "0")).toLong()
     }
-}
 
 fun advent1p2(input: String): Long {
     return input.lowercase().split("\n").fold(0L) { acc, s ->
-        val stringDigit = s.findAnyOf(digitStrings)
-        val charDigit = s.findAnyOf(digitChar)
-        val firstDigit = when {
-            stringDigit == null && charDigit == null -> 0
-            stringDigit == null -> digitChar.indexOf(charDigit!!.second)
-            charDigit == null -> digitStrings.indexOf(stringDigit.second)
-            charDigit.first < stringDigit.first -> digitChar.indexOf(charDigit.second)
-            else -> digitStrings.indexOf(stringDigit.second)
-        }
-        val lastStringDigit = s.findLastAnyOf(digitStrings)
-        val lastCharDigit = s.findLastAnyOf(digitChar)
-        val lastDigit = when {
-            lastStringDigit == null && lastCharDigit == null -> 0
-            lastStringDigit == null -> digitChar.indexOf(lastCharDigit!!.second)
-            lastCharDigit == null -> digitStrings.indexOf(lastStringDigit.second)
-            lastCharDigit.first > lastStringDigit.first -> digitChar.indexOf(lastCharDigit.second)
-            else -> digitStrings.indexOf(lastStringDigit.second)
-        }
-        acc + firstDigit * 10 + lastDigit
+        acc +
+                listOfNotNull(
+                    s.findAnyOf(digitChar),
+                    s.findAnyOf(digitStrings)
+                ).minByOrNull { it.first }?.second.toDigit() * 10 +
+                listOfNotNull(
+                    s.findLastAnyOf(digitChar),
+                    s.findLastAnyOf(digitStrings)
+                ).maxByOrNull { it.first }?.second.toDigit()
     }
 }
+
+private fun String?.toDigit(): Int = this?.toIntOrNull() ?: max(0, digitStrings.indexOf(this))
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,16 @@
 import data.*
 
 fun main() {
-    println("Program result: ${adventDay7Part2()}")
+    println("Program result: ${adventDay1Part2()}")
 }
+
+private fun adventDay1Part1Test(): Long = advent1p1(ADVENT_DAY_1_PART_1_DATA_TEST) // 142
+
+private fun adventDay1Part1(): Long = advent1p1(ADVENT_DAY_1_DATA) //55712
+
+private fun adventDay1Part2Test(): Long = advent1p2(ADVENT_DAY_1_PART_2_DATA_TEST) // 281
+
+private fun adventDay1Part2(): Long = advent1p2(ADVENT_DAY_1_DATA) // 55413
 
 private fun adventDay5Part2(): Long = advent5p2(ADVENT_DAY_5_DATA)
 
